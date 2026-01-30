483a7e706efa66c66e207b38c58ce1344464f09e
ilies9360 <109932593+ilies9360@users.noreply.github.com>
2026-01-30 17:17:08 +0100

    Update LigueConsole.java
    On peux maintenant ajouter la date de départ et d'arriveé en creant un employé

--- Personnel/src/commandLine/LigueConsole.java
+++ Personnel/src/commandLine/LigueConsole.java
@@ -2,6 +2,7 @@ package commandLine;
 
 import static commandLineMenus.rendering.examples.util.InOut.getString;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import commandLineMenus.List;
@@ -99,7 +100,8 @@ public class LigueConsole
 				{
 					ligue.addEmploye(getString("nom : "), 
 						getString("prenom : "), getString("mail : "), 
-						getString("password : "));
+						getString("password : "),LocalDate.parse(getString("date d'arrivée (yyyy-mm-dd) : ")),
+						LocalDate.parse(getString("date  (yyyy-mm-dd) : ")));
 				}
 		);
 	}
