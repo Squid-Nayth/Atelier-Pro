308e387e826589ba88100146eae7ef3893a6863f
Lucressefz <fortunezeufack@yahoo.com>
2026-02-06 13:06:25 +0100

    Update Script.sql

--- Personnel/script.sql
+++ Personnel/script.sql
@@ -9,11 +9,10 @@ CREATE TABLE EMPLOYE (
     Nom VARCHAR(50) NOT NULL,
     Prenom VARCHAR(50) NOT NULL,
     Password VARCHAR(255) NOT NULL,
-    role_employe VARCHAR(50) NOT NULL,
     Date_arrivee DATE NOT NULL,
     Date_depart DATE,
     num_ligue_appartenir INT,
     num_ligue_administrer INT,
     
     CONSTRAINT fk_employe_appartenir FOREIGN KEY (num_ligue_appartenir) REFERENCES LIGUE(num_ligue),
-    CONSTRAINT fk_employe_administrer FOREIGN KEY (num_ligue_administrer) REFERENCES LIGUE(num_ligue));
+    CONSTRAINT fk_employe_administrer FOREIGN KEY (num_ligue_administrer) REFERENCES LIGUE(num_ligue));''g
